4f1977ce52d1d633b9745ec4e59fc53a7425699d
danik <danmalkov@yandex.ru>
2015-06-29 00:36:41 +0300

    Пздц я ее запустил наконец то

--- ActionExecutors/src/Main.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Даник
- * on 28.06.2015.
- */
-public class Main {
-
-}
---
+++ Service/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Даник
+ * on 28.06.2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
